b081769417f89677de84994304fbac349f72153c
nitin1813 <nitin.kumar.84625@gmail.com>
2023-07-28 15:49:15 +0530

    some changes to trainSchedule

---
+++ trailnSchedule/train/src/components/Footer.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+
+function Footer(){
+    return(
+        <div className="footer">
+
+            <p>@copyright 2023</p>
+
+        </div>
+    )
+}
+
+
+export default Footer;
---
+++ trailnSchedule/train/src/components/Header.js
@@ -0,0 +1,14 @@
+import react from "react";
+import train from  "./../img/train.jpeg"
+
+function Header(){
+    return(
+        <div className="footer">
+        <img id="trainImage" src={train} alt="Train Image"></img>
+           
+        </div>
+    )
+}
+
+
+export default Header;
